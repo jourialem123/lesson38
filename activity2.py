afcbc8fc673092b1eb4c54554892893d706762d0
jourialem123 <alemjouri@gmail.com>
2025-01-11 20:36:19 +0300

    activity2

---
+++ activity2.py
@@ -0,0 +1,20 @@
+#Store two numbers of your choice in two different variables.
+#  Now perform all the Arithmetic operations on those two numbers and print the results.
+
+num1=50
+num2=20
+
+#Arithretic operators
+print("Sum of ",num1," and ",num2," is ",num1+num2)
+print("Difference of ",num1," and ",num2," is ",num1-num2)
+print("Product of ",num1," and ",num2," is ",num1*num2)
+print("Quotient of ",num1," divided by ",num2," is ",num1/num2)
+print("Floor Quotient of ",num1," divided by ",num2," is ",num1//num2)
+print("Remainder of ",num1," divided by ",num2," is ",num1%num2)
+result=num1/5*num2**3+5
+print("Result of expression is ",result)
+
+#Conditional  operators
+print("Is ",num1,"equal to",num2,num1==num2)
+print("Is ",num1,"greater than ",num2,num1>num2)
+print("Is ",num1,"less than ",num2,num1<num2)
